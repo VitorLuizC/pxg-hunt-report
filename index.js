8931804dfec6a7740ffb889ca889735e33d5c652
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2016-12-04 16:04:24 -0200

    Loot almost done + structure changed

--- index.js
+++ index.js
@@ -1,20 +1,79 @@
 "use strict";
 
-const breakOn = require("./breakOn.js");
-const areNumbers = require("./areNumbers.js");
+const { breakOn, areNumbers } = require("./lib");
 
+// 21:55 Miles Kane: Venusaur loot: a bag of pollem, bottles of poison (7) and seeds (5).
 
 /**
- * Get Loot.
  * @param {string} line
- * @param {boolean} hasTimestamp
- * @returns {Array<Object>}
+ * @returns {boolean}
+ */
+function hasTimestamp(line) {
+  return (areNumbers(line[0], line[1], line[3], line[4]) && line[2] === ":" && line[5] === " ");
+}
+
+exports.hasTimestamp = hasTimestamp;
+
+/**
+ * Get Timestamp.
+ * @param {string} line
+ * @returns {string}
+ */
+function getTimestamp(line) {
+  if (!hasTimestamp(line))
+    return null;
+  return line.substring(0, 5);
+}
+
+exports.getTimestamp = getTimestamp;
+
+/**
+ * @param {string} line
+ */
+function getPlayer(line) {
+  if (hasTimestamp(line))
+    return line.substring(6, line.indexOf(": "));
+  return line.substring(0, line.indexOf(": "));
+}
+
+exports.getPlayer = getPlayer;
+
+/**
+ * @param {string} line
+ */
+function getPokemon(line) {
+  var player = getPlayer(line);
+  var start = player.length + ": ".length;
+
+  if (hasTimestamp(line))
+    start += 6;
+  return line.substring(start, line.indexOf(" loot: ", start));
+}
+
+/**
+ * Get list of all pokémon loot.
+ * @param {string} line
  */
 function getLoot(line) {
-  var text = line.substring(0, line.lastIndexOf("."));
+  var player = getPlayer(line);
+  var pokemon = getPokemon(line);
+  var start = player.length + ": ".length + pokemon.length + " loot: ".length;
 
-  return breakOn(text, ", ", " and ");
+
+  if (hasTimestamp(line))
+    start += 6;
+
+  line = line.substring(start, line.indexOf(".", start));
+
+  var loots = [];
+  var items = line.split(", ");
+
+  items.forEach(item => item.split(" and ").forEach(item => loots.push(item)));
+
+  return loots;
 }
+
+exports.getLoot = getLoot;
 
 /**
  * Generate a object with all data report.
@@ -25,36 +84,18 @@ function generateReport(text) {
   var lines = breakOn(text, "\n");
 
   var data = lines.map(line => {
-    let timestamp, player, pokemon, loot;
-    const hasTimestamp = (areNumbers(line[0], line[1], line[3], line[4]) && line[2] === ":" && line[5] === " ");    
-    const getTimestamp = line => line.substring(0, 5);
-    const getPlayer = line => line.substring(0, line.indexOf(":", 6));
-    const getPokemon = line => line.substring(0, line.indexOf(" loot: "));
-    const removeTimestamp = line => line.substring(6);
-    const removePlayer = line => line.substring(player.length + ": ".length);
-    const removePokemon = line => line.substring(pokemon.length + " loot: ".length);
-
-    if (hasTimestamp) {
-      timestamp = getTimestamp(line);
-      line = removeTimestamp(line);
-    }
-
-    player = getPlayer(line);
-    line = removePlayer(line);
-
-    pokemon = getPokemon(line);
-    line = removePokemon(line);
-
     return {
-      Timestamp: timestamp || null,
-      Player: player || null,
-      Pokemon: pokemon || null,
+      Timestamp: getTimestamp(line),
+      Player: getPlayer(line),
+      Pokemon: getPokemon(line),
       Loot: getLoot(line)
     };
   });
 
   return data;
 }
+
+exports.generateReport = generateReport;
 
 var report = generateReport(`
 23:35 Miles Kane: Magikarp loot: a water gem.
